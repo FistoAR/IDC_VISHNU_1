c90dcdc5c3ea43bc76f673dd7674219bc0358156
VISHNUKUMAR2716 <vishnuarun2704@gmail.com>
2026-01-03 12:38:26 +0530

    Fix SVG text alignment in editor

--- idc/src/App.jsx
+++ idc/src/App.jsx
@@ -1,11 +1,12 @@
 import PhotopeaEditor from "./components/Propertiespanel";
 import Toolbar from "./components/Toolbar"
 import Layerespanel from "./components/Layerspanel"
+import ImportExport from "./components/ImportExport"
 export default function App() {
   return (
     <div className="App">
 
-<Layerespanel />
+<ImportExport />
 
     </div>
   );
